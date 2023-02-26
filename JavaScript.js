ac4a113cd9a4cc2a7025bbee5bbf487faff6fbbb
langry254 <brianmwallo21@gmail.com>
2023-02-26 13:53:30 +0300

    Add project files.

---
+++ JavaScript.js
@@ -0,0 +1,55 @@
+﻿window.onload = function () {
+    // Add a new entry to the history stack
+    window.history.pushState(null, null, window.location.href);
+
+    // Replace the current entry with the new one
+    window.history.replaceState(null, null, window.location.href);
+    //validating the login
+}
+    function validateLogin() {
+        var name = document.getElementById("email").value;
+        var pass = document.getElementById("pass1").value;
+        if (name == "" || pass == "") {
+            alert("Please fill in all fields.");
+            return false;
+        }
+    }
+    // validating the sign up
+    function validateSignup() {
+        var f_names = document.getElementById("f-names").value;
+        var mail = document.getElementById("email1").value;
+        var pas1 = document.getElementById("pas").value;
+        var pass2 = document.getElementById("pas2").value;
+        if (pas1 != pass2) {
+            alert("passwords dont match");
+        }
+        else if (pas1.length < 6) {
+            alert("Password should six characters or more")
+        }
+
+        if (f_names == "" || mail == "" || pas1 == "" || pass2 == "") {
+            alert("Please fill in all fields.");
+            return false;
+        }
+        else {
+            window.location.href = 'forgotpassword.html';
+        }
+    }
+    //validating the reset page
+    function validateReset() {
+        var r_pas1 = document.getElementById("r_pass").value;
+        var r_pass2 = document.getElementById("r_pass1").value;
+        if (r_pas1 != r_pass2) {
+            alert("passwords dont match");
+        }
+        else if (r_pas1.length < 6) {
+            alert("Password should six characters or more")
+        }
+        else {
+            location.href = 'Signup.html';
+        }
+
+
+
+    }
+
---
+++ Login.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="Grocery_store.Login" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>login page</title>
+    <link rel="stylesheet" type="text/css" href="login.css" />
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+    </form>
+</body>
+</html>
